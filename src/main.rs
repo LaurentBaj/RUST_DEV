13ed28f746ac41a7db601ad970434b8fa9237a7a
LaurentBaj <laurent_baj@hotmail.com>
2021-12-10 02:39:46 +0100

    Basic calc added
    Playing around with functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-mod print; 
+//mod print; 
+mod math;
 
 fn main() {
-    print::run();
+    let x = math::add(2, 2);
+    let y = math::subtract(10, 6);
+    let z = math::multiply(2, 2);
+    let o = math::divide(16.8, 4.2); // Has to be f32
+
+    println!("{}", x); 
+    println!("{}", y); 
+    println!("{}", z);
+    println!("{}", o);
 }
---
+++ src/math.rs
@@ -0,0 +1,7 @@
+pub fn add(num1: i32, num2: i32) -> i32 { return num1 + num2; } 
+
+pub fn subtract(num1: i32, num2: i32) -> i32 { return num1 - num2; } 
+
+pub fn multiply(num1: i32, num2: i32) -> i32 { return num1 * num2; } 
+
+pub fn divide(num1: f32, num2: f32) -> f32 { return (num1 as f32) / (num2 as f32); } 
