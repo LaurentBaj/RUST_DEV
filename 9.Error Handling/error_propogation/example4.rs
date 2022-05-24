10c730f67078b08e34f9b5f69d8f0c508d06d80e
LaurentBaj <laurent_baj@hotmail.com>
2022-05-24 20:41:19 +0200

    Chapter 9 refac

--- 9.Error Handling/error_propogation/example4.rs
+++ 9.Error Handling/error_propogation/example4.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::io;
 
-// The most verbose version of all the other examples
+// The least verbose version of all the other examples
 // Note that we have to import the entire 'fs' module
 fn read_username_from_file() -> Result<String, io::Error> {
     fs::read_to_string("hello.txt")
