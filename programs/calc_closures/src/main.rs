1612f1d9c340db727182a2034ddd087937b2744a
laurent_ Bajrami <LaurentBaj>
2022-06-28 20:18:05 +0200

    Closure with calculator example

--- programs/calc_closures/src/main.rs
+++ programs/calc_closures/src/main.rs
@@ -5,10 +5,10 @@ fn calculate(x: i32, y: i32, op: fn(i32, i32) -> i32) -> i32 {
 
 fn main() {
     // Closures
-    let add = |x: i32, y: i32| x + y;
-    let sub = |x: i32, y: i32| x - y;
-    let mul = |x: i32, y: i32| x * y;
-    let div = |x: i32, y: i32| x / y;
+    let add = |x, y| x + y;
+    let sub = |x, y| x - y;
+    let mul = |x, y| x * y;
+    let div = |x, y| x / y;
 
     // calculate() takes in two i32 and a fn which in this case is anonymous
     assert_eq!(calculate(2, 2, add), 4);
