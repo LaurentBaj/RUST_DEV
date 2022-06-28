28c38cdd60802169163653c56f4400c790701051
laurent_ Bajrami <LaurentBaj>
2022-06-28 15:10:41 +0200

    Closure with calculator example

--- programs/calc_closures/src/lib.rs
+++ programs/calc_closures/src/lib.rs
@@ -1,9 +1,19 @@
 #[allow(dead_code)]
-
-
 fn calculate(x: i32, y: i32, op: fn(i32, i32) -> i32) -> i32 {
     op(x, y)
 }
+
+#[allow(dead_code)]
+fn add(x: i32, y: i32) -> i32 { x + y }
+
+#[allow(dead_code)]
+fn sub(x: i32, y: i32) -> i32 { x - y }
+
+#[allow(dead_code)]
+fn mult(x: i32, y:i32) -> i32 { x * y }
+
+#[allow(dead_code)]
+fn div(x: i32, y: i32) -> i32 { x / y }
 
 
 #[cfg(test)]
@@ -12,25 +22,25 @@ mod tests {
 
     #[test]
     fn test_addtion() {
-        let x = calculate(2, 2, |x, y| x + y);
-        assert_eq!(x, 4);
+        let res = calculate(2, 2, add);
+        assert_eq!(res, 4);
     }
 
     #[test]
     fn test_subtraction() {
-        let x = calculate(2, 2, |x, y| x - y);
-        assert_eq!(x, 0);
+        let res = calculate(2, 2, sub);
+        assert_eq!(res, 0);
     }
 
     #[test]
     fn test_multiplication() {
-        let x = calculate(2, 2, |x, y| x * y);
-        assert_eq!(x, 4);
+        let res = calculate(21, 21, mult);
+        assert_eq!(res, 441);
     }
 
     #[test]
     fn test_division() {
-        let x = calculate(2, 2, |x, y| x / y);
-        assert_eq!(x, 1);
+        let res = calculate(81, 9, div);
+        assert_eq!(res, 9);
     }
 }
