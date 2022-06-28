b70705fc2101f1beb7ba94a1f060a0475b92fb9a
laurent_ Bajrami <LaurentBaj>
2022-06-28 21:43:25 +0200

    Recreated shoe program from chapter 13 with a twist!

---
+++ programs/shoe_program/src/lib.rs
@@ -0,0 +1,46 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    name: String,
+    size: u8
+}
+
+impl Shoe {
+    #[allow(dead_code)]
+    fn new(name: String, size: u8) -> Shoe {
+        Shoe { 
+            name, 
+            size
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn shoes_that_fit_me(shoes: Vec<Shoe>, shoe_size: u8) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tes_shoe_that_fit_me() {
+        let shoes: Vec<Shoe> = vec![
+            Shoe::new(String::from("Nike"), 43),
+            Shoe::new(String::from("Adidas"), 42),
+            Shoe::new(String::from("Umbro"), 43)
+        ];
+
+        let for_me = shoes_that_fit_me(shoes, 43);
+
+        assert_eq!(
+            for_me,
+            vec![
+                Shoe::new(String::from("Nike"), 43),
+                Shoe::new(String::from("Umbro"), 43)
+            ]
+        )
+    }
+}
