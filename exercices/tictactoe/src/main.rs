984adf94e456e8ebc9ff3bdd7cdee8620ed84679
laurent_ Bajrami <LaurentBaj>
2023-01-15 19:47:19 +0100

    TicTacToe part 2

--- exercices/tictactoe/src/main.rs
+++ exercices/tictactoe/src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     println!("\nWelcome to Tic Tac Toe!\n");
     print_board(&mut slots);
 
-    // Make sure every other player turn
+    // Make sure it's every other player turn
     for i in 0..9 {
+        // check_winner();
         let turn = match i % 2 { 0 => "O", _ => "X" };
         mark_slot(turn, &mut slots);
     }
@@ -15,12 +16,26 @@ fn main() {
 //// Read player input and mark a slot
 fn mark_slot(player: &str, slots: &mut [String; 9]) {
     println!("'{}'s turn\n", player);
-    let input = user_input();
-    
-    // Mark slot with 'X' or 'O'
+    let input = user_input();    
     let input_num = input.parse::<usize>().unwrap() - 1;
+    let current = &slots[input_num];
+
+    if current == "O" || current == "X" {
+        println!("Slot is already taken!");
+        mark_slot(player, slots);
+    }
+
     slots[input_num] = String::from(player);
+    check_winner(player, slots);
     print_board(slots);
+}
+
+fn check_winner(player: &str, slots: &mut [String; 9]) {
+    let player = String::from(player);
+
+    if slots[0] == player && slots[1] == player && slots[2] == player {
+        println!("{} is the winner!", player);
+    }
 }
 
 // Read input, trim it and convert it to a string
